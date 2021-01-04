cdb84c8c4d400317a5e3324d67a9c5dde2d6f3fb
unknown <v-lydialiu@microsoft.com>
2021-01-04 03:27:23 +0000

    DataBreakpointTest

---
+++ ConsoleApp4/ConsoleApp4/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace ConsoleApp4
+{
+    class Program
+    {
+        string person { set; get; }
+        static void Main(string[] args)
+        {
+            int x = 1;
+            Program A = new Program();
+            A.person = "Alisa";//set bp
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
